1471381a9c0ed191124174e8c7456ee94ee998a3
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-07-01 12:30:15 +0530

    feat: new plugins

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,14 @@
+---@diagnostic disable: lowercase-global
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  require 'custom.plugins.toggleterm',
+  require 'custom.plugins.cokeline',
+  require 'custom.plugins.alpha',
+  require 'custom.plugins.typescript-tools',
+  require 'custom.plugins.tsc',
+  require 'custom.plugins.themes',
+  require 'custom.plugins.tmux',
+}
